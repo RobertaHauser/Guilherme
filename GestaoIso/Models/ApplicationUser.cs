86cb3f059b9486dbdf3a4be248f142ee4a92c8c6
RobertaBGH <roberta.hauser@outlook.com>
2022-04-20 12:50:32 -0300

    Adicionar arquivos de projeto.

---
+++ GestaoIso/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GestaoIso.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+
+    }
+}
